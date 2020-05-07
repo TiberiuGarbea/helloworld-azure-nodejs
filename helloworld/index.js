fff78774282bc3f9d1222195f17e986d276bd02b
TiberiuGarbea <TiberiuGarbea@users.noreply.github.com>
2020-05-07 12:41:19 +0300

    Update index.js

--- helloworld/index.js
+++ helloworld/index.js
@@ -1,9 +1,9 @@
 module.exports = async function (context, req) {
-
+ context.log('JavaScript HTTP trigger function processed a request.' + context.invocationId)
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
-            body: "Hello " + (req.query.name || req.body.name),
-            context.log('JavaScript HTTP trigger function processed a request.' + context.invocationId)
+            body: "Hello " + (req.query.name || req.body.name)
+           
         };
     }
     else {
